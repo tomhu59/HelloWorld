2d18ead6499b9e7f74b50832bfe56e3774cfd0e2
tomhu59 <baichuanhu2008@gmail.com>
2017-01-17 21:26:38 +0800

    added HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package com.cavalierit;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 * In command line run the program:
+	 * C:\Users\Tom\Documents\workpolace\Test5\src>java com.cavalierit.HelloWorld
+	 */
+	public static void main(String[] args) {
+		for (int i=1; i<=10; i++){
+			System.out.println("Hello World " + i);
+			
+			
+		}
+
+	}
+
+}
